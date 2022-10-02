2305c2dc29cae539e806819f7927e22f0b354e23
whatbee <77581924+whatbee@users.noreply.github.com>
2022-10-02 19:48:00 +0100

    fixed second bug
    trying to cd into current directory headass

--- Unix/newdir.sh
+++ Unix/newdir.sh
@@ -1,12 +1,11 @@
 ## creates set of dirs based on name of practical ##
-echo  "Enter name of practical, with underscores (no spaces)"
-read pracName                                          # receive input from user, store in variable $pracName
-dirName=("$(date "+%Y-%m-%d")-$pracName")              # combine formatted date with $pracName to create dirName
-mkdir $dirName                                         # create a directory with name determined by dirName
-cd $dirName                                            # enter said directory
-mkdir input                                            # create "input" directory
-mkdir tmp                                              # create "tmp" directory
-mkdir results                                          # create "results" directory
-touch WHATIDID.TXT                                     # create empty WHATIDID.txt without entering it
-#cd $dirName                                           # not sure why this doesn't work, cba to fix
-echo "directory $dirName and files created."
+echo  "Enter name of practical, with underscores (no spaces):"
+read pracName                             # receive input from user, store in variable $pracName
+dirName=("$(date "+%Y-%m-%d")-$pracName") # combine formatted date with $pracName to create dirName
+mkdir $dirName                            # create a directory with name determined by dirName
+cd $dirName                               # enter said directory
+mkdir input                               # create "input" directory
+mkdir tmp                                 # create "tmp" directory
+mkdir results                             # create "results" directory
+touch WHATIDID.TXT                        # create empty WHATIDID.txt without entering it
+echo "Directory $dirName and files created."
