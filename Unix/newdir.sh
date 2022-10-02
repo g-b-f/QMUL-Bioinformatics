37bb1b9dc490f27a2ccba4838e58cfaecb910321
whatbee <77581924+whatbee@users.noreply.github.com>
2022-10-02 15:47:44 +0100

    Added comments

--- Unix/newdir.sh
+++ Unix/newdir.sh
@@ -1,13 +1,12 @@
-#creates set of dirs based on name of practical
+## creates set of dirs based on name of practical ##
 echo  "Enter name of practical, with underscores (no spaces)"
-read pracName
-dirName=("$(date '+%Y-%m-%d')-$pracName") 2> /dev/null #supress spurious error thrown by date
-#echo $dirName
-mkdir $dirName
-cd $dirName
-mkdir input
-mkdir tmp
-mkdir results
-touch WHATIDID.TXT
-#cd $dirName #not sure why this doesn't work, cba to fix
-echo dir $dirName and files created.
+read pracName                                          # receive input from user, store in variable $pracName
+dirName=("$(date "+%Y-%m-%d")-$pracName") 2> /dev/null # combine formatted date with $pracName to create dirName, send error messages to null to ignore them
+mkdir $dirName                                         # create a directory with name determined by dirName
+cd $dirName                                            # enter said directory
+mkdir input                                            # create "input" directory
+mkdir tmp                                              # create "tmp" directory
+mkdir results                                          # create "results" directory
+touch WHATIDID.TXT                                     # create empty WHATIDID.txt without entering it
+#cd $dirName                                           # not sure why this doesn't work, cba to fix
+echo "directory $dirName and files created."
